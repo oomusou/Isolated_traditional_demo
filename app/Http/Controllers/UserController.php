5f2b437c25cf8c592b69368d1aa6b112e90a50fe
oomusou <oomusou@gmail.com>
2015-11-14 21:17:03 +0800

    1. 修改route 2. 新增UserController 3. 新增UserTableSeeder 4. 新增users/index/index.blade.php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+}
